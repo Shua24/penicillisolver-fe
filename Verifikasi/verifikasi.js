e81792c81ffba558eae99eda06460d7a3f6c6258
myousefg <myousefg@gmail.com>
2024-10-17 23:39:08 +0700

    menambahkan beranda dan tentangpolakuman, testing setiap file, memperbaiki navbar dan verifikasi

--- Verifikasi/verifikasi.js
+++ Verifikasi/verifikasi.js
@@ -1,3 +1,31 @@
+window.onload = function () {
+    const inputs = document.querySelectorAll(".code-input");
+
+    inputs.forEach((input, index) => {
+        input.addEventListener("input", (e) => {
+            const value = e.target.value;
+            if (value.length === 1 && index < inputs.length - 1) {
+                inputs[index + 1].focus();
+            } else if (value.length === 0 && index > 0) {
+                inputs[index - 1].focus();
+            }
+        });
+
+        input.addEventListener("keydown", (e) => {
+            if (e.key === "Backspace" && index > 0 && e.target.value === "") {
+                inputs[index - 1].focus();
+            }
+        });
+    });
+};
+
+function clearInputs() {
+    document.querySelectorAll(".code-input").forEach(input => {
+        input.value = "";
+    });
+    document.getElementById("code1").focus();
+}
+
 function verifikasi() {
     const code1 = document.getElementById("code1").value;
     const code2 = document.getElementById("code2").value;
@@ -7,19 +35,17 @@ function verifikasi() {
     const otp = code1 + code2 + code3 + code4;
 
     if (otp === "1234") {
-        document.getElementById("pesan").innerText = "Nah gitu dong, dari tadi kek...";
+        document.getElementById("pesan").innerText = "Kode OTP Terverifikasi!";
         document.getElementById("pesan").style.color = "#008080";
         setTimeout(function() {
             window.location.href = '../Beranda/beranda.html';
         }, 2000);
-    } else if(otp === "") {
-        document.getElementById("pesan").innerText = "Isi OTPnya AJG !";
+    } else if (otp === "") {
+        document.getElementById("pesan").innerText = "Kode OTP Kosong!";
         document.getElementById("pesan").style.color = "red";
-        highlightErrorInputs();
     } else {
-        document.getElementById("pesan").innerText = "isi yg bener ASW !";
+        document.getElementById("pesan").innerText = "Kode OTP Salah!";
         document.getElementById("pesan").style.color = "red";
-        highlightErrorInputs();
+        clearInputs();
     }
-    
 }
