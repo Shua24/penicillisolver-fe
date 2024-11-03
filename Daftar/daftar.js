ab888c393efc1fff84b5b694ee19d4c9f4cefdd9
Muhammad Ghiyats Fatiha <daffaadriantoe23@gmail.com>
2024-11-03 18:28:46 +0700

    Menamabahkan file js pada Daftar dan Login

--- Daftar/daftar.js
+++ Daftar/daftar.js
@@ -1,23 +1,23 @@
 document.querySelector("form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Mencegah submit form agar tidak reload halaman
+    event.preventDefault(); // biar gk reload
 
-    // Ambil data dari input form
+    // ngambil data dari inputan form
     const nama = document.getElementById("nama").value;
     const email = document.getElementById("email").value;
     const sip = document.getElementById("sip").value;
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirm_password").value;
 
-    // Cek apakah Password dan Konfirmasi Password cocok
+    // ngecek biar sama pssword sama confirmpassword
     if (password !== confirmPassword) {
         alert("Password dengan Konfirmasi Password tidak cocok!");
-        return; // Hentikan proses jika password tidak cocok
+        return; // stop klo misalkan salah
     }
 
-    // Jika password cocok, simpan data ke localStorage dalam bentuk JSON
+    // buat nyimpen data user sementara
     const userData = { nama, email, sip, password };
     localStorage.setItem("userData", JSON.stringify(userData));
 
-    // Redirect ke halaman login
+    // ke login.html
     window.location.href = "../Login/login.html";
 });
--- Login/login.js
+++ Login/login.js
@@ -1,21 +1,21 @@
 document.querySelector("form").addEventListener("submit", function(event) {
-    event.preventDefault(); // Mencegah form submit untuk validasi
+    event.preventDefault(); 
 
-    // Ambil nilai input dari form
+    // data apa aja yg diambil buat login
     const emailInput = document.getElementById("email").value;
     const passwordInput = document.getElementById("password").value;
 
-    // Ambil data pengguna yang tersimpan di localStorage
+    // ngambil data user di localStorage
     const userData = JSON.parse(localStorage.getItem("userData"));
 
-    // Validasi apakah pengguna terdaftar atau tidak
+    // buat nyari data user yang udah terdaftar
     if (!userData || userData.email !== emailInput) {
         alert("User belum terdaftar!");
     } else if (userData.password !== passwordInput) {
         alert("Kata sandi salah!");
     } else {
         alert("Login berhasil!");
-        // Arahkan ke halaman beranda jika login berhasil
+       
         window.location.href = "../Beranda/beranda.html";
     }
 });
